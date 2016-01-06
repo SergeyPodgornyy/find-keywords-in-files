87251b350bba3bb4dc347c31a5adb3233894a907
Sergey Podgornyy <sergey.podgornyy@yahoo.de>
2016-01-06 22:35:15 +0200

    Clean code and write documentation

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Store folder content into array
 i=0
 while read line
 do
@@ -7,49 +8,44 @@ do
 	(( i++ ))
 done < <(ls fich/)
 
+# Define keywords which we will use for search
 KEYWORDS=( NoSQL C++ Anglais Rigueur ITIL Django jQuery Python PHP HTML CodeIgniter ERP C# XML Ajax JavaScript UML SQL C Doxygen VBA SharePoint MySQL MVC iOS Android WinRT )
 
 # The following were deleted for an array:
 # - base de donnees
 # - Pack Office
 
+# Write header of file
 str="Name of file"
 for val in "${KEYWORDS[@]}"
 do
-	str=$str:$val
+	str=$str'|'$val
 done
 echo $str>result
-
-# grep -H
-# Print  the  file  name  for  each  match.   This is the default when there is more than one file to search.
 
 # grep -o
 # Print  only the matched (non-empty) parts of a matching line, with each such part on a separate output line.
 
 # grep -w
-# Select  only  those  lines containing matches that form whole words.  The test is that the  matching  substring must  either  be  at  the  beginning  of  the  line, or preceded   by   a   non-word   constituent   character. Similarly,  it must be either at the end of the line or followed by a non-word  constituent  character.   Word-constituent  characters  are  letters,  digits, and the underscore.
+# Select  only  those  lines containing matches that form whole words.
 
 # grep -c
-# Suppress  normal  output;  instead  print  a  count  of matching  lines  for  each  input  file.   With the -v, --invert-match option (see below),  count  non-matching lines.
+# Suppress  normal  output;  instead  print  a  count  of matching  lines  for  each  input  file.
 
 # grep -i
 # Ignore  case  distinctions  in both the PATTERN and the input files.
 
-
+# Iterate through files in array
 n=0
 while [ $n -le $i ] 
 do
-	name=$(echo ${array[n]})
-	for word in "${KEYWORDS[@]}" 
+	name=$(echo ${array[n]}) # Write name of file
+	for word in "${KEYWORDS[@]}" # Iterate through keywords
 	do
-		ser=$(grep -wo "$word" fich/"${array[n]}" | grep -c $word)
-		count=$count:$ser
+		ser=$(grep -woi "$word" fich/"${array[n]}" | grep -c $word) # Search count of keywordsin file
+		count=$count'|'$ser # Concatenate string with number of matched words
 	done
-	echo $name$count>>result 2>/dev/null
-	unset count
+	echo $name$count>>result 2>/dev/null # Push result (standart output)  to a file. Standart error will send to /dev/null (to trash)
+	unset count # unset 'count' variable for this iteration
 	(( n++ ))
 done
-
-
-
-
